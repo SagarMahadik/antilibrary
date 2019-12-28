181f02d75b415c485879208162d818978d058f34
sgrmhdk51 <sgrmhdk51@gmail.com>
2019-12-28 21:50:07 +0530

    fixed the bug in model

--- models/fancyItemModel.js
+++ models/fancyItemModel.js
@@ -23,52 +23,68 @@ const fancyItemSchema = new mongoose.Schema({
         required:[true,"Please enter notable work"]
     },
     quotes:[
-        {
-            quote :{
-                type:String,
-                unique:true
+        {   quote :{
+
+            type:String,
+            sparse:true
             }
         }
+         
     ],
     books:[
         {
-            name:{
+            bookName:{
                 
                 type:String,
-                unique:true
+                sparse:true
             },
             bookURL :{
 
-                type: String
+                type: String,
+                sparse:true
             }
         }
     ],
     videos:[
         {
-            name:{
+            videoName:{
                 type:String,
-                maxlength:[50,"A video description must have less or equal to 50 characters"]
+                maxlength:[50,"A video description must have less or equal to 50 characters"],
+                sparse:true
             },
             videoURL: {
-                type:String
+                type:String,
+                sparse:true
             }
         }
     ],
     courses:[
         {
-            name:{
-                type:String
+            courseName:{
+                type:String,
+                sparse:true
             },
             courseURL: {
-                type:String
+                type:String,
+                sparse:true
             },
             platform :{
-                type:String
+                type:String,
+                sparse:true
             }
         }
     ]
 
 });
+
+/*fancyItemSchema.pre("save",function(next){
+    if(this.books.length===0) this.books.push("There is no book for thisfancyITem"); 
+    if(this.quotes.length===0) this.quotes.push("There is no quote for this fancyitem");
+    if (this.courses.length === 0)this.courses.push("There is no coursesfor this fancyitem"); 
+    if(this.videos.length ===0)this.videos.push("There is no video for this fancyitem");    
+    next();  
+     
+})*/
 
 const FancyItem = mongoose.model('FancyItem',fancyItemSchema);
 
